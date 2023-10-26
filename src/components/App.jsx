16fd591636330b8c938fb533d5996a4329ef25e2
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-10-26 15:40:24 +0200

    task 04 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import data from './data.json';
 import Statistics from './Statistics/Statistics';
 import friends from './friends.json';
 import FriendList from './FriendList/FriendList';
+import transactions from './transactions.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -19,8 +21,11 @@ export const App = () => {
       {/* ????? */}
       {/* <Statistics stats={data}/> */}
     
-      <FriendList friends={friends}/>
+      <FriendList friends={friends} />
+      
+      <TransactionHistory items={transactions} />
     </>
+
   );
 };
 
